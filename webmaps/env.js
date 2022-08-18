f1037f01f0df8c477c4815675c4b45f55665d4f7
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2022-08-18 09:21:49 -0300

    Tentativa

---
+++ webmaps/env.js
@@ -0,0 +1,7 @@
+const THUNDER_KEY = '939a323d64294a95a4413662bc8514ba'
+const MAPBOX_KEY = 'pk.eyJ1Ijoia3lsZWZlbGlwZSIsImEiOiJja2dicDJ3ODgwajVqMnh0ajNuYWNndHQzIn0.9VVmCAO-vNhPc0_T1DC8Zg'
+const INITIAL_VIEW = {
+  lagoa_santa: [-19.62699243, -43.88995792],
+  default: [-3.130409, -60.0203426]
+};
+const INITIAL_ZOOM = 15;
